5ad331b782930683c6056bc2ff5159169ab686c7
Kendrick Elsey <K.Elsey@cbanewzealand.onmicrosoft.com>
2017-11-01 10:29:36 +1300

    Added a comment to highlight that the options class is currently read only.

--- WebApp/Options/CBAOptions.cs
+++ WebApp/Options/CBAOptions.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 
 namespace WebApp.Options
 {
+    /// <summary>
+    /// This class binds to the configuration values in the database.
+    /// It is currently read only. Modified properties won't be reflected in the database.
+    /// </summary>
     public class CBAOptions
     {
         // public parameterless constructor required in order to act as a service for DI
