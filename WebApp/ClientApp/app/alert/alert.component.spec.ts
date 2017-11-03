e9c9700d70310024c787bb780c984307a02a414b
Kendrick Elsey <K.Elsey@cbanewzealand.onmicrosoft.com>
2017-11-04 10:33:00 +1300

    Updates to unit tests.

--- WebApp/ClientApp/app/alert/alert.component.spec.ts
+++ WebApp/ClientApp/app/alert/alert.component.spec.ts
@@ -1,25 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
+import { AlertService } from "./alert.service";
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AlertComponent', () => {
-  let component: AlertComponent;
-  let fixture: ComponentFixture<AlertComponent>;
+    let component: AlertComponent;
+    let fixture: ComponentFixture<AlertComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AlertComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            providers: [AlertService],
+            declarations: [AlertComponent],
+            imports: [RouterTestingModule]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AlertComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AlertComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- WebApp/ClientApp/app/invoices/invoicelist.component.spec.ts
+++ WebApp/ClientApp/app/invoices/invoicelist.component.spec.ts
@@ -1,25 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { InvoiceService } from './invoice.service';
 import { InvoicelistComponent } from './invoicelist.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('InvoicelistComponent', () => {
-  let component: InvoicelistComponent;
-  let fixture: ComponentFixture<InvoicelistComponent>;
+    let component: InvoicelistComponent;
+    let fixture: ComponentFixture<InvoicelistComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ InvoicelistComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [InvoicelistComponent],
+            providers: [InvoiceService],
+            imports: [RouterTestingModule]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InvoicelistComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(InvoicelistComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
