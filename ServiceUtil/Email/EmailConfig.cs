618298811ea3986647ef9f3834b0d03c3c355f3c
Helerson Lage <h.lage@cbanewzealand.onmicrosoft.com>
2017-12-04 23:35:23 +1300

    Created a class can send mail and attachments.

---
+++ ServiceUtil/Email/EmailConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceUtil.Email
+{
+    public class EmailConfig : IEmailConfig
+    {
+        public String FromName { get; set; }
+        public String FromAddress { get; set; }
+
+        public String LocalDomain { get; set; }
+
+        public String MailServerAddress { get; set; }
+        public int MailServerPort { get; set; }
+
+        public String UserId { get; set; }
+        public String UserPassword { get; set; }
+    }
+}
