94fbe680578fb047ac17464381676f3122d7c679
Midhu <midhucherian@gmail.com>
2018-01-19 12:40:49 +1300

    changes

--- WebApp/ClientApp/app/invoices/invoicelist.component.spec.ts
+++ WebApp/ClientApp/app/invoices/invoicelist.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { InvoiceService } from './invoice.service';
 import { InvoicelistComponent } from './invoicelist.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('InvoicelistComponent', () => {
   let component: InvoicelistComponent;
@@ -8,7 +9,9 @@ describe('InvoicelistComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InvoicelistComponent ]
+        declarations: [InvoicelistComponent],
+         providers: [InvoiceService],
+        imports: [RouterTestingModule]
     })
     .compileComponents();
   }));
