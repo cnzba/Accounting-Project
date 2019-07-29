4e6dbaecd88b8b16591a8ac5066beacaa7f4964b
Henry <henryyue2010@gmail.com>
2019-07-29 17:39:56 +1200

    fixed issues

--- WebApp/ClientApp/src/app/pipes/invoice-filter.pipe.ts
+++ WebApp/ClientApp/src/app/pipes/invoice-filter.pipe.ts
@@ -1,10 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Pipe({
   name: 'invoiceFilter'
 })
 
 export class InvoiceFilterPipe implements PipeTransform {
+
+    constructor(public datepipe: DatePipe) { }
 
     transform(value: any[], searchString: string) {
 
@@ -17,9 +20,13 @@ export class InvoiceFilterPipe implements PipeTransform {
             const clientName = it.clientName.toLocaleLowerCase().includes(searchString.toLocaleLowerCase())
             const status = it.status.toLowerCase().includes(searchString.toLowerCase())
             const invoiceNumber = it.invoiceNumber.toLowerCase().includes(searchString.toLowerCase())
-            const grandTotal = it.grandTotal.toString().toLowerCase().includes(searchString.toLowerCase())
-            const dateCreated = it.dateCreated.toString().toLowerCase().includes(searchString.toLowerCase())
-            const dateDue = it.dateDue.toString().toLowerCase().includes(searchString.toLowerCase())
+            const grandTotal = ("$"+it.grandTotal.toString()).toLowerCase().includes(searchString.toLowerCase())
+
+            let format_dateCreated = this.datepipe.transform(it.dateCreated, 'dd/MM/yyyy');
+            const dateCreated = format_dateCreated.toString().toLowerCase().includes(searchString.toLowerCase())
+
+            let format_dateDue = this.datepipe.transform(it.dateDue, 'dd/MM/yyyy');
+            const dateDue = format_dateDue.toString().toLowerCase().includes(searchString.toLowerCase())
                   
             return (clientName + status + invoiceNumber + grandTotal + dateCreated + dateDue);
         });
